57751793bafc19f30391438ccd4fb47c900875fe
Nods2008 <noor.daglhas@gmail.com>
2021-03-07 18:01:41 +0200

    Final Review, hope this doesnt cost money

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('zHello World! Nours First App in AWS!'));
+app.get('/', (req, res) => res.send('Testing Review Stage!zHello World! Nours First App in AWS!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
